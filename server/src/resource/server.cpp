988b922162504b889d2d27b3f52aa81462fc8075
cyberc001 <cyb3rc001@yandex.ru>
2024-11-04 11:09:42 +0300

    Added /servers GET method.

--- server/src/resource/server.cpp
+++ server/src/resource/server.cpp
@@ -1,10 +1,27 @@
 #include "resource/server.h"
+#include <nlohmann/json.hpp>
 
 #include <iostream>
 
 server_resource::server_resource(connection_pool& pool, auth_resource& auth): pool{pool}, auth{auth}
 {}
 
+std::shared_ptr<http_response> server_resource::render_GET(const http_request& req)
+{
+	session_token token;
+	auto err = auth.parse_session_token(req, token);
+	if(err) return err;
+	int user_id = auth.sessions[token];
+
+	auto conn = pool.hold();
+	pqxx::work tx{*conn};
+
+	pqxx::result r = tx.exec_params("SELECT server_id FROM user_x_server WHERE user_id = $1", user_id);
+	nlohmann::json res;
+	for(size_t i = 0; i < r.size(); ++i)
+		res += r[i]["server_id"].as<int>();
+	return std::shared_ptr<http_response>(new string_response(res.dump(), 200));
+}
 std::shared_ptr<http_response> server_resource::render_POST(const http_request& req)
 {
 	std::string_view name = req.get_header("name");
@@ -21,12 +38,15 @@ std::shared_ptr<http_response> server_resource::render_POST(const http_request& 
 	if(r.size() >= owned_per_user)
 		return std::shared_ptr<http_response>(new string_response("Too much servers owned by user", 403));
 	
+	int server_id;
 	try{
 		r = tx.exec_params("INSERT INTO servers(name, owner_id) VALUES($1, $2) RETURNING server_id", name, user_id);
+		server_id = r[0]["server_id"].as<int>();
+		tx.exec_params("INSERT INTO user_x_server(user_id, server_id) VALUES($1, $2)", user_id, server_id);
 	} catch(pqxx::data_exception& e){
 		return std::shared_ptr<http_response>(new string_response("Server name is too long", 400));
 	}
 	tx.commit();
 
-	return std::shared_ptr<http_response>(new string_response(std::to_string(r[0]["server_id"].as<int>()), 200));
+	return std::shared_ptr<http_response>(new string_response(std::to_string(server_id), 200));
 }
--- server/src/resource/server.h
+++ server/src/resource/server.h
@@ -8,6 +8,7 @@ class server_resource : public http_resource
 {
 public:
 	server_resource(connection_pool& pool, auth_resource& auth);
+	std::shared_ptr<http_response> render_GET(const http_request&);
 	std::shared_ptr<http_response> render_POST(const http_request&);
 
 	unsigned owned_per_user = 2;
