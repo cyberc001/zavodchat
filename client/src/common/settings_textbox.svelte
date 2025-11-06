5eba4a0e6de280f3d72da1783e9055fc8f06aaee
cyberc001 <cyb3rc001@yandex.ru>
2025-11-06 19:11:35 +0300

    basis for official client in Svelte.

---
+++ client/src/common/settings_textbox.svelte
@@ -0,0 +1,34 @@
+<script>
+	const {label_text, is_password = false} = $props();
+</script>
+
+<input autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" type={is_password ? "password" : "input"} class="settings_textbox"/>
+<p class="settings_label">{label_text}</p>
+
+<style>
+.settings_textbox {
+	font-size: 20px;
+	color: #F6F5F5;
+
+	background: #2A2E34;
+
+	border-width: 2px;
+	border-style: solid;
+	border-radius: 4px;
+}
+.settings_textbox:focus {
+	outline: none;
+
+	border-color: #C8C0C0;
+	border-style: solid;
+}
+.settings_textbox::selection {
+	background: #644646;
+}
+
+.settings_label {
+	text-align: left;
+	margin: 0;
+	font-size: 16px;
+}
+</style>
---
+++ client/src/login.svelte
@@ -0,0 +1,14 @@
+<script>
+	import SettingsTextbox from './common/settings_textbox.svelte';
+</script>
+
+<div class="login_frame">
+<p style="margin: 0; margin-bottom: 20px;">Log in</p>
+<SettingsTextbox label_text="username"/>
+<p style="margin: 0; margin-bottom: 6px;"/>
+<SettingsTextbox label_text="password" is_password=true/>
+</div>
+
+<style>
+	@import "login.css";
+</style>
